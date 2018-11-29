ca9d299fb263799893c561ca9a13c50eab481784
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-11-29 11:58:50 +0200

    Vector implementation #1
    Initial implementation of the CC_VECTOR  data structure.
    
    Error: corrupted heap

--- data_struct/ccvector.c
+++ data_struct/ccvector.c
@@ -3,63 +3,220 @@
 
 int VecCreate(CC_VECTOR **Vector)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    CC_VECTOR *vec = (CC_VECTOR *)malloc(sizeof(CC_VECTOR));
+
+    if (NULL == vec)
+    {
+        return -1;
+    }
+
+    vec->Array = (int *)malloc(sizeof(int));
+
+    if (NULL == vec->Array)
+    {
+        free(vec);
+        return -1;
+    }
+
+    vec->Count = 0;
+    vec->Size = 1;
+
+    *Vector = vec;
+
+    return 0;
 }
 
 int VecDestroy(CC_VECTOR **Vector)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    CC_VECTOR *vec = *Vector;
+
+    free(vec->Array);
+    free(*Vector);
+
+    *Vector = NULL;
+
+    return 0;
 }
 
 int VecInsertTail(CC_VECTOR *Vector, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    if (Vector->Size == Vector->Count)
+    {
+        int *arr = (int *)realloc(Vector->Array, sizeof(int) * Vector->Size * 2);
+
+        if (NULL == arr)
+        {
+            return -1;
+        }
+
+        Vector->Array = arr;
+    }
+
+    Vector->Array[Vector->Count++] = Value;
+    return 0;
 }
 
 int VecInsertHead(CC_VECTOR *Vector, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    if (Vector->Size == Vector->Count)
+    {
+        int *arr = (int *)realloc(Vector->Array, sizeof(int) * Vector->Size * 2);
+
+        if (NULL == arr)
+        {
+            return -1;
+        }
+
+        Vector->Array = arr;
+    }
+
+    for (int i = Vector->Count; i > 0; i--)
+    {
+        Vector->Array[i] = Vector->Array[i - 1];
+    }
+
+    Vector->Array[0] = Value;
+    Vector->Count++;
+
+    return 0;
 }
 
 int VecInsertAfterIndex(CC_VECTOR *Vector, int Index, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Index);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    if (Index < -1 || Index >= Vector->Count)
+    {
+        return -1;
+    }
+
+    if (Vector->Size == Vector->Count)
+    {
+        int *arr = (int *)realloc(Vector->Array, sizeof(int) * Vector->Size * 2);
+
+        if (NULL == arr)
+        {
+            return -1;
+        }
+
+        Vector->Array = arr;
+    }
+
+    for (int i = Vector->Count; i > Index + 1; i--)
+    {
+        Vector->Array[i] = Vector->Array[i - 1];
+    }
+
+    Vector->Array[Index + 1] = Value;
+    Vector->Count++;
+
+    return 0;
 }
 
 int VecRemoveByIndex(CC_VECTOR *Vector, int Index)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Index);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    if (Index < 0 && Index >= Vector->Count)
+    {
+        return -1;
+    }
+
+    for (int i = Index; i < Vector->Count - 1; i++)
+    {
+        Vector->Array[i] = Vector->Array[i + 1];
+    }
+
+    Vector->Count--;
+
+    if (Vector->Count == Vector->Size / 4)
+    {
+        int *arr = (int *)realloc(Vector->Array, sizeof(int) * Vector->Size / 2);
+
+        if (NULL == arr)
+        {
+            return -1;
+        }
+
+        Vector->Array = arr;
+    }
+
+    return 0;
 }
 
 int VecGetValueByIndex(CC_VECTOR *Vector, int Index, int *Value)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    CC_UNREFERENCED_PARAMETER(Index);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Vector || NULL == Value)
+    {
+        return -1;
+    }
+
+    if (Index < 0 || Index >= Vector->Count)
+    {
+        return -1;
+    }
+
+    *Value = Vector->Array[Index];
+
+    return 0;
 }
 
 int VecGetCount(CC_VECTOR *Vector)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    return Vector->Count;
 }
 
 int VecClear(CC_VECTOR *Vector)
 {
-    CC_UNREFERENCED_PARAMETER(Vector);
-    return -1;
+    if (NULL == Vector)
+    {
+        return -1;
+    }
+
+    int *arr = (int *)malloc(sizeof(int));
+
+    if (NULL == arr)
+    {
+        return -1;
+    }
+
+    free(Vector->Array);
+    Vector->Array = arr;
+    Vector->Size = 1;
+    Vector->Count = 0;
+
+    return 0;
 }
 
 int VecSort(CC_VECTOR *Vector)
--- data_struct/ccvector.h
+++ data_struct/ccvector.h
@@ -1,18 +1,20 @@
 #pragma once
 
-typedef struct _CC_VECTOR { 
+typedef struct _CC_VECTOR {
     // Members
-    int PlaceHolder; // placeholder to be removed when actual implementation is added
-} CC_VECTOR; 
-  
-int VecCreate(CC_VECTOR **Vector); 
-int VecDestroy(CC_VECTOR **Vector); 
-  
-int VecInsertTail(CC_VECTOR *Vector, int Value); 
-int VecInsertHead(CC_VECTOR *Vector, int Value); 
-int VecInsertAfterIndex(CC_VECTOR *Vector, int Index, int Value); 
-int VecRemoveByIndex(CC_VECTOR *Vector, int Index);  
-int VecGetValueByIndex(CC_VECTOR *Vector, int Index, int *Value);  
-int VecGetCount(CC_VECTOR *Vector);  
-int VecClear(CC_VECTOR *Vector);  
-int VecSort(CC_VECTOR *Vector);  
+    int *Array;
+    int Size;
+    int Count;
+} CC_VECTOR;
+
+int VecCreate(CC_VECTOR **Vector);
+int VecDestroy(CC_VECTOR **Vector);
+
+int VecInsertTail(CC_VECTOR *Vector, int Value);
+int VecInsertHead(CC_VECTOR *Vector, int Value);
+int VecInsertAfterIndex(CC_VECTOR *Vector, int Index, int Value);
+int VecRemoveByIndex(CC_VECTOR *Vector, int Index);
+int VecGetValueByIndex(CC_VECTOR *Vector, int Index, int *Value);
+int VecGetCount(CC_VECTOR *Vector);
+int VecClear(CC_VECTOR *Vector);
+int VecSort(CC_VECTOR *Vector);
