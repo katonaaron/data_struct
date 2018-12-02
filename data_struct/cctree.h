8bdf9818082002edcbc524ea6695c46b52a902e2
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-12-02 21:48:15 +0200

    Tree declaration

--- data_struct/cctree.h
+++ data_struct/cctree.h
@@ -1,9 +1,25 @@
 #pragma once
 
+//The Red-Black tree algorithm is used for this data structure.
+
+//Two constants define the color of the node
+typedef enum _CC_TREE_NODE_COLOR {
+    BLACK,
+    RED
+} CC_TREE_NODE_COLOR;
+
+//Node of the tree
+typedef struct _CC_TREE_NODE {
+    struct _CC_TREE_NODE *Parent, *Left, *Right;
+    int Value;
+    CC_TREE_NODE_COLOR Color;
+} CC_TREE_NODE, *PCC_TREE_NODE;
+
 typedef struct _CC_TREE {
     // Members
-    int PlaceHolder; // placeholder to be removed when actual implementation is added
-} CC_TREE;
+    PCC_TREE_NODE Root;
+    int Count;
+} CC_TREE, *PCC_TREE;
 
 int TreeCreate(CC_TREE **Tree);
 int TreeDestroy(CC_TREE **Tree);
