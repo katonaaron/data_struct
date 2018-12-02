d2c0e697719cbc203591ee7f3de2efc83084caa3
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-12-02 22:44:43 +0200

    Tree implementation #1
    -TreeCreate, TreeCreateNode and TreeInsert functions were implemented
    
    The following prototypes were created:
    -TreeCreateNode and TreeDestroyNode: manage the nodes of the tree
    -TreeInsertRepair repairs the tree after insertion

--- data_struct/cctree.c
+++ data_struct/cctree.c
@@ -1,23 +1,97 @@
 #include "cctree.h"
 #include "common.h"
 
+static int TreeCreateNode(CC_TREE_NODE **Node, int Value);
+static int TreeDestroyNode(CC_TREE_NODE **Node);
+
+//Repairs the tree to preserve the red-black tree structure, starting from the inserted node;
+static int TreeInsertRepair(CC_TREE_NODE *Node);
+
 int TreeCreate(CC_TREE **Tree)
 {
-    CC_UNREFERENCED_PARAMETER(Tree);
-    return -1;
+    if (NULL == Tree)
+    {
+        return -1;
+    }
+
+    PCC_TREE tree = malloc(sizeof(CC_TREE));
+
+    if (NULL == tree)
+    {
+        *Tree = NULL;
+        return -1;
+    }
+
+    tree->Count = 0;
+    tree->Root = NULL;
+
+    *Tree = tree;
+
+    return 0;
 }
 
 int TreeDestroy(CC_TREE **Tree)
 {
-    CC_UNREFERENCED_PARAMETER(Tree);
+    if (NULL == Tree)
+    {
+        return -1;
+    }
+
+
     return -1;
 }
 
 int TreeInsert(CC_TREE *Tree, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Tree);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Tree)
+    {
+        return -1;
+    }
+
+
+    PCC_TREE_NODE node;
+    PCC_TREE_NODE it = Tree->Root;  //iterator
+
+    if (0 != TreeCreateNode(&node, Value))
+    {
+        return -1;
+    }
+
+    if (NULL == Tree->Root)
+    {
+        Tree->Root = node;
+    }
+    else
+    {
+        while (NULL == node->Parent)
+        {
+            if (it->Value > Value)
+            {
+                if (NULL == it->Left)
+                {
+                    it->Left = node;
+                    node->Parent = it;
+                }
+                else
+                {
+                    it = it->Left;
+                }
+            }
+            else {
+                if (NULL == it->Right)
+                {
+                    it->Right = node;
+                    node->Parent = it;
+                }
+                else
+                {
+                    it = it->Right;
+                }
+            }
+        }
+    }
+  
+    return 0;
 }
 
 int TreeRemove(CC_TREE *Tree, int Value)
@@ -76,3 +150,43 @@ int TreeGetNthPostorder(CC_TREE *Tree, int Index, int *Value)
     return -1;
 }
 
+static int TreeCreateNode(CC_TREE_NODE **Node, int Value)
+{
+    if (NULL == Node)
+    {
+        return -1;
+    }
+
+    PCC_TREE_NODE node = malloc(sizeof(CC_TREE_NODE));
+
+    if (NULL == node)
+    {
+        *Node = NULL;
+        return -1;
+    }
+
+    node->Parent = NULL;
+    node->Left = NULL;
+    node->Right = NULL;
+    node->Color = RED;
+    node->Value = Value;
+    *Node = node;
+
+    return 0;
+}
+
+int TreeDestroyNode(CC_TREE_NODE ** Node)
+{
+    if (NULL == Node)
+    {
+        return -1;
+    }
+
+
+    return -1;
+}
+
+int TreeInsertRepair(CC_TREE_NODE * Node)
+{
+    return -1;
+}
