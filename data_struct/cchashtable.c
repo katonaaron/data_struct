777d3dd18cce1f3581c1a500163cc456b269a4ca
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-12-05 18:05:20 +0200

    Hashtable implementation #3
    -HtKeyCopy and HtKeyLength string functions were implemented
    -HtDestroy and HtClear updated - must free the allocated keys
    -HtSetKeyValue saves the first empty spot, and returns -1 if founds the same key. Copies the key to a newly allocated memory.
    -HtRemoveKey, HtGetNthKey, HtHash, implemented

--- data_struct/cchashtable.c
+++ data_struct/cchashtable.c
@@ -1,5 +1,7 @@
 #include "cchashtable.h"
 #include "common.h"
+#include <limits.h>
+
 
 //VecResize increments or decrements the size of the dynamically allocated array
 //int Up: selects the incrementation or the decrementation operation
@@ -16,6 +18,12 @@ static int HtHash(char *Key);
 //  0:	the contents of both strings are equal
 //  >0:	the first character that does not match has a greater value in Key1 than in Key2
 static int HtKeyCompare(char *Key1, char *Key2);
+
+//Copies the string from KeySource to KeyDestination
+static void HtKeyCopy(char *KeySource, char *KeyDestination);
+
+//Returns the length of the string
+static int HtKeyLength(char *Key);
 
 //HtIsPrime determines if a given number is a prime number
 static int HtIsPrime(int Number);
@@ -54,9 +62,14 @@ int HtDestroy(CC_HASH_TABLE** HashTable)
         return -1;
     }
 
-    free((*HashTable)->Data);
+    if (-1 == HtClear(*HashTable))
+    {
+        return -1;
+    }
+
     free(*HashTable);
 
+    *HashTable = NULL;
     return 0;
 }
 
@@ -67,12 +80,8 @@ int HtSetKeyValue(CC_HASH_TABLE* HashTable, char* Key, int Value)
         return -1;
     }
 
-    if (0 != HtHasKey(HashTable, Key))
+    if (0 == HashTable->Size)
     {
-        return -1;
-    }
-
-    if (0 == HashTable->Size || (HashTable->Count + 1.0) / HashTable->Size > CC_MAX_LOAD_FACTOR) {
         if (-1 == HtResize(HashTable, 1))
         {
             return -1;
@@ -81,13 +90,43 @@ int HtSetKeyValue(CC_HASH_TABLE* HashTable, char* Key, int Value)
 
     int hash = HtHash(Key);
     int index = hash % HashTable->Size;
+    int firstEmpty = -1;
 
-    for (int i = 0; NULL != HashTable->Data[index].Key; i++)
+    for (int i = 0; NULL != HashTable->Data[index].Key || 0 != HashTable->Data[index].Value; i++)
     {
+        if (NULL == HashTable->Data[index].Key)
+        {
+            firstEmpty = index;
+        }
+        if (NULL != HashTable->Data[index].Key && 0 == HtKeyCompare(HashTable->Data[index].Key, Key))
+        {
+            return -1;
+        }
         index = (hash + i * i) % HashTable->Size;
     }
 
-    HashTable->Data[index].Key = Key;
+    if (firstEmpty != -1)
+    {
+        index = firstEmpty;
+    }
+
+    if ((HashTable->Count + 1.0) / HashTable->Size > CC_MAX_LOAD_FACTOR) {
+        if (-1 == HtResize(HashTable, 1))
+        {
+            return -1;
+        }
+    }
+
+    char *key = (char *)malloc(HtKeyLength(Key) * sizeof(char));
+
+    if (NULL == key)
+    {
+        return -1;
+    }
+
+    HtKeyCopy(Key, key);
+
+    HashTable->Data[index].Key = key;
     HashTable->Data[index].Value = Value;
     HashTable->Count++;
 
@@ -108,7 +147,8 @@ int HtGetKeyValue(CC_HASH_TABLE* HashTable, char* Key, int *Value)
     {
         if (NULL != HashTable->Data[index].Key && 0 == HtKeyCompare(HashTable->Data[index].Key, Key))
         {
-            return HashTable->Data[index].Value;
+            *Value = HashTable->Data[index].Value;
+            return 0;
         }
         index = (hash + i * i) % HashTable->Size;
     }
@@ -117,8 +157,34 @@ int HtGetKeyValue(CC_HASH_TABLE* HashTable, char* Key, int *Value)
 
 int HtRemoveKey(CC_HASH_TABLE* HashTable, char* Key)
 {
-    CC_UNREFERENCED_PARAMETER(HashTable);
-    CC_UNREFERENCED_PARAMETER(Key);
+    if (NULL == HashTable || NULL == Key)
+    {
+        return -1;
+    }
+
+    int hash = HtHash(Key);
+    int index = hash % HashTable->Size;
+
+    for (int i = 0; NULL != HashTable->Data[index].Key || 0 != HashTable->Data[index].Value; i++)
+    {
+        if (NULL != HashTable->Data[index].Key && 0 == HtKeyCompare(HashTable->Data[index].Key, Key))
+        {
+            free(HashTable->Data[index].Key);
+            HashTable->Data[index].Key = NULL;
+            HashTable->Data[index].Value = -1;
+            HashTable->Count--;
+
+            if (((float)HashTable->Count) / HashTable->Size < CC_MIN_LOAD_FACTOR) {
+                if (-1 == HtResize(HashTable, 0))
+                {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+        index = (hash + i * i) % HashTable->Size;
+    }
     return -1;
 }
 
@@ -145,10 +211,23 @@ int HtHasKey(CC_HASH_TABLE* HashTable, char* Key)
 
 int HtGetNthKey(CC_HASH_TABLE* HashTable, int Index, char** Key)
 {
-    CC_UNREFERENCED_PARAMETER(HashTable);
-    CC_UNREFERENCED_PARAMETER(Index);
-    CC_UNREFERENCED_PARAMETER(Key);
-    return -1;
+    if (NULL == HashTable || NULL == Key || Index < 0 || Index >= HashTable->Count)
+    {
+        return -1;
+    }
+
+    int index = 0;
+    for (int i = 0; index < Index + 1; i++)
+    {
+        if (NULL != HashTable->Data[i].Key)
+        {
+            index++;
+        }
+    }
+
+    *Key = HashTable->Data[index].Key;
+
+    return 0;
 }
 
 int HtClear(CC_HASH_TABLE* HashTable)
@@ -156,6 +235,18 @@ int HtClear(CC_HASH_TABLE* HashTable)
     if (NULL == HashTable)
     {
         return -1;
+    }
+
+    int count = 0;
+    for (int i = 0; count < HashTable->Count; i++)
+    {
+        if (NULL != HashTable->Data[i].Key)
+        {
+            free(HashTable->Data[i].Key);
+            HashTable->Data[i].Key = NULL;
+            HashTable->Data[i].Value = -1;
+            count++;
+        }
     }
 
     free(HashTable->Data);
@@ -185,8 +276,8 @@ static int HtResize(CC_HASH_TABLE *HashTable, int Up)
         if (0 == HashTable->Size)
         {
             newSize = 2;
-        } 
-        else 
+        }
+        else
         {
             newSize = HtNextPrime(HashTable->Size * 2);
         }
@@ -235,12 +326,24 @@ static int HtResize(CC_HASH_TABLE *HashTable, int Up)
 
 static int HtHash(char *Key)
 {
-    return -1;
+    int hash = 0;
+    for (char *i = Key; *i; i++)
+    {
+        hash += 31 * *i;
+    }
+
+    if (hash < 0)
+    {
+        hash += INT_MAX;
+    }
+
+    return hash;
 }
 
-int HtKeyCompare(char * Key1, char * Key2)
+static int HtKeyCompare(char * Key1, char * Key2)
 {
-    for (char *i = Key1; *i; i++)
+    char *i = Key1;
+    while (*i)
     {
         if (*i > *(Key2 + (i - Key1)))
         {
@@ -250,8 +353,39 @@ int HtKeyCompare(char * Key1, char * Key2)
         {
             return -1;
         }
+        i++;
+    };
+
+    if (*i > *(Key2 + (i - Key1)))
+    {
+        return 1;
     }
+    else if (*i < *(Key2 + (i - Key1)))
+    {
+        return -1;
+    }
+
     return 0;
+}
+
+static void HtKeyCopy(char *KeySource, char *KeyDestination)
+{
+    int i;
+    for (i = 0; KeySource[i]; i++)
+    {
+        KeyDestination[i] = KeySource[i];
+    }
+    KeyDestination[i] = 0;
+}
+
+static int HtKeyLength(char *Key)
+{
+    int i = 0;
+    while (Key[i])
+    {
+        i++;
+    }
+    return i + 1;
 }
 
 static int HtIsPrime(int Number)
