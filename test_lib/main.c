ec3bc9811e7c5640952a5047cec980508d2e5a06
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-12-06 00:45:30 +0200

    Tree test #1
    -A structure test was created (evaluated using the debug tool)
    -More tests are needed!

--- test_lib/main.c
+++ test_lib/main.c
@@ -86,6 +86,22 @@ int TestTree()
         goto cleanup;
     }
 
+    //Structure Test
+
+    int nodes[] = { 43, 18, 22, 9, 21, 6, 8, 20, 63, 50, 62, 51, 6 };
+
+    for (int i = 0; i < 13; i++)
+    {
+        retVal = TreeInsert(usedTree, nodes[i]);
+        if (0 != retVal)
+        {
+            printf("TreeInsert failed!: line: %d\n", __LINE__);
+            goto cleanup;
+        }
+    }
+
+
+    //End Structure Test
     retVal = TreeInsert(usedTree, 20);
     if (0 != retVal)
     {
