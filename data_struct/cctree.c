767d1c4eb6c0085242a5e377d35038eecf8f9ec8
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-12-06 13:12:36 +0200

    Tree implementation #3 - Remove
    -The Remove function was implemented
    -TreeRemoveNoed, TreeFindNode, TreeGetMinNode, TreeGetBalanceFactor functions were implemented. They are needed for TreeRemove.
    -TreeGetCount was implemented
    -TreeGetNthPreorder and the TreeTemporaryPreorder functions are temprorarily implemented for debug purposes
    -TreeDestroyNode - can delete recursively or just a single node

--- data_struct/cctree.c
+++ data_struct/cctree.c
@@ -2,10 +2,22 @@
 #include "common.h"
 
 static int TreeCreateNode(CC_TREE_NODE **Node, int Value);
-static int TreeDestroyNode(CC_TREE_NODE **Node);
+static int TreeDestroyNode(CC_TREE_NODE **Node, int Recursively);
+
+
+//TreeRemoveNode removes a node from the tree
+static int TreeRemoveNode(CC_TREE *Tree, CC_TREE_NODE *Node);
+
+//TreeGetMinNode gives the Node parameter the address of the node with the value = Value, from the 
+//subtree given by the Root node.
+static int TreeFindNode(CC_TREE_NODE *Root, int Value, CC_TREE_NODE **Node);
 
 //TreeGetHeight returns the height of the (sub)tree where Node is the root
 static int TreeGetNodeHeight(CC_TREE_NODE *Node);
+
+//TreeGetMinNode gives the Node parameter the address of the node with the smallest value in the 
+//subtree given by the Root node.
+static int TreeGetMinNode(CC_TREE_NODE *Root, CC_TREE_NODE **Node);
 
 //TreeRotateLeft does a left rotation
 static int TreeRotateLeft(CC_TREE *Tree, CC_TREE_NODE *Node);
@@ -16,6 +28,10 @@ static int TreeRotateRight(CC_TREE *Tree, CC_TREE_NODE *Node);
 //TreeBalance makes rotations in order to balance the tree.
 //The Node will be the starting point, from where we go up and check the other nodes;
 static int TreeBalance(CC_TREE *Tree, CC_TREE_NODE *Node);
+
+//TreeGetBalanceFactor returns the balance factor of the node:
+//  height(left child) - hight(right child)
+static int TreeGetBalanceFactor(CC_TREE_NODE *Node);
 
 //Returns the maximum of the given 2 integers
 static int Max(int Num1, int Num2);
@@ -52,7 +68,7 @@ int TreeDestroy(CC_TREE **Tree)
 
     PCC_TREE tree = *Tree;
 
-    if (-1 == TreeDestroyNode(&tree->Root))
+    if (-1 == TreeDestroyNode(&tree->Root, 1))
     {
         return -1;
     }
@@ -118,9 +134,39 @@ int TreeInsert(CC_TREE *Tree, int Value)
 
 int TreeRemove(CC_TREE *Tree, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Tree);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Tree)
+    {
+        return -1;
+    }
+
+    PCC_TREE_NODE foundNode = NULL;
+
+    if (0 != TreeFindNode(Tree->Root, Value, &foundNode))
+    {
+        return -1;
+    }
+
+    int found = 0;
+
+    while (NULL != foundNode)
+    {
+        if (0 != TreeRemoveNode(Tree, foundNode))
+        {
+            return -1;
+        }
+        if (0 != TreeFindNode(Tree->Root, Value, &foundNode))
+        {
+            return -1;
+        }
+        found = 1;
+        Tree->Count--;
+    }
+
+    if (!found)
+    {
+        return -1;
+    }
+    return 0;
 }
 
 int TreeContains(CC_TREE *Tree, int Value)
@@ -132,8 +178,12 @@ int TreeContains(CC_TREE *Tree, int Value)
 
 int TreeGetCount(CC_TREE *Tree)
 {
-    CC_UNREFERENCED_PARAMETER(Tree);
-    return -1;
+    if (NULL == Tree)
+    {
+        return -1;
+    }
+
+    return Tree->Count;
 }
 
 int TreeGetHeight(CC_TREE *Tree)
@@ -152,12 +202,34 @@ int TreeClear(CC_TREE *Tree)
     return -1;
 }
 
+static void TreeTemporaryPreorder(CC_TREE_NODE *Node, int *Index, int *Value)
+{
+    if (*Index == 0)
+    {
+        *Value = Node->Value;
+        return;
+    }
+    if (Index > 0 && NULL != Node->Left)
+    {
+        (*Index)--;
+        TreeTemporaryPreorder(Node->Left, Index, Value);
+    }
+    if (Index > 0 && NULL != Node->Right)
+    {
+        (*Index)--;
+        TreeTemporaryPreorder(Node->Right, Index, Value);
+    }
+}
+
 int TreeGetNthPreorder(CC_TREE *Tree, int Index, int *Value)
 {
-    CC_UNREFERENCED_PARAMETER(Tree);
-    CC_UNREFERENCED_PARAMETER(Index);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Tree || NULL == Value || Index < 0 || Index >= Tree->Count)
+    {
+        return -1;
+    }
+
+    TreeTemporaryPreorder(Tree->Root, &Index, Value);
+    return 0;
 }
 
 int TreeGetNthInorder(CC_TREE *Tree, int Index, int *Value)
@@ -201,28 +273,131 @@ static int TreeCreateNode(CC_TREE_NODE **Node, int Value)
     return 0;
 }
 
-int TreeDestroyNode(CC_TREE_NODE ** Node)
+int TreeDestroyNode(CC_TREE_NODE **Node, int Recursively)
 {
     if (NULL == Node)
     {
         return -1;
     }
 
-    PCC_TREE_NODE node = *Node;
-
-    if (NULL != node->Left && 0 != TreeDestroyNode(&node->Left))
+    if (Recursively)
     {
-        return -1;
-    }
+        PCC_TREE_NODE node = *Node;
 
-    if (NULL != node->Right && 0 != TreeDestroyNode(&node->Right))
-    {
-        return -1;
+        if (NULL != node->Left && 0 != TreeDestroyNode(&node->Left, 1))
+        {
+            return -1;
+        }
+
+        if (NULL != node->Right && 0 != TreeDestroyNode(&node->Right, 1))
+        {
+            return -1;
+        }
     }
 
     free(*Node);
     *Node = NULL;
 
+    return 0;
+}
+
+int TreeRemoveNode(CC_TREE *Tree, CC_TREE_NODE *Node)
+{
+    if (NULL == Node || NULL == Tree)
+    {
+        return -1;
+    }
+
+    PCC_TREE_NODE parent = Node->Parent;
+
+    if (NULL == Node->Left || NULL == Node->Right)
+    {
+        PCC_TREE_NODE child = Node->Left ? Node->Left : Node->Right;
+
+        if (NULL == child)
+        {
+            if (NULL == parent)
+            {
+                Tree->Root = NULL;
+            }
+            else if (parent->Left == Node)
+            {
+                parent->Left = NULL;
+            }
+            else
+            {
+                parent->Right = NULL;
+            }
+
+            if (0 != TreeDestroyNode(&Node, 0))
+            {
+                return -1;
+            }
+        }
+        else
+        {
+            Node->Left = child->Left;
+            Node->Right = child->Right;
+            Node->Value = child->Value;
+            Node->Height = child->Height;
+
+            if (0 != TreeDestroyNode(&child, 0))
+            {
+                return -1;
+            }
+        }
+    }
+    else
+    {
+        PCC_TREE_NODE minNode = NULL;
+        if (0 != TreeGetMinNode(Node->Right, &minNode) || NULL == minNode)
+        {
+            return -1;
+        }
+
+        Node->Value = minNode->Value;
+
+        if (0 != TreeRemoveNode(Tree, minNode))
+        {
+            return -1;
+        }
+    }
+
+    if (NULL != parent)
+    {
+        return TreeBalance(Tree, parent);
+    }
+
+    return 0;
+}
+
+int TreeFindNode(CC_TREE_NODE *Root, int Value, CC_TREE_NODE ** Node)
+{
+    if (NULL == Root || NULL == Node)
+    {
+        return -1;
+    }
+
+    if (Root->Value > Value)
+    {
+        if (NULL == Root->Left)
+        {
+            *Node = NULL;
+            return 0;
+        }
+        return TreeFindNode(Root->Left, Value, Node);
+    }
+    else if (Root->Value < Value)
+    {
+        if (NULL == Root->Right)
+        {
+            *Node = NULL;
+            return 0;
+        }
+        return TreeFindNode(Root->Right, Value, Node);
+    }
+
+    *Node = Root;
     return 0;
 }
 
@@ -233,6 +408,25 @@ int TreeGetNodeHeight(CC_TREE_NODE *Node)
         return 0;
     }
     return Node->Height;
+}
+
+int TreeGetMinNode(CC_TREE_NODE * Root, CC_TREE_NODE ** Node)
+{
+    if (NULL == Root || NULL == Node)
+    {
+        return -1;
+    }
+
+    if (NULL == Root->Left)
+    {
+        *Node = Root;
+    }
+    else
+    {
+        TreeGetMinNode(Root->Left, Node);
+    }
+
+    return 0;
 }
 
 static int TreeRotateLeft(CC_TREE *Tree, CC_TREE_NODE *Node)
@@ -251,7 +445,7 @@ static int TreeRotateLeft(CC_TREE *Tree, CC_TREE_NODE *Node)
     {
         Tree->Root = child;
     }
-    else 
+    else
     {
         if (child->Parent->Left == Node)
         {
@@ -297,7 +491,7 @@ static int TreeRotateRight(CC_TREE *Tree, CC_TREE_NODE *Node)
             child->Parent->Right = child;
         }
     }
-   
+
 
     Node->Left = child->Right;
     child->Right = Node;
@@ -315,18 +509,16 @@ static int TreeBalance(CC_TREE *Tree, CC_TREE_NODE *Node)
         return -1;
     }
 
-    int leafValue = Node->Value;
-
     while (Node)
     {
-        int balance = TreeGetNodeHeight(Node->Left) - TreeGetNodeHeight(Node->Right);
+        int balance = TreeGetBalanceFactor(Node);
         Node->Height = 1 + Max(TreeGetNodeHeight(Node->Left), TreeGetNodeHeight(Node->Right));
 
 
         if (balance > 1)
         {
-            
-            if (Node->Left->Value >= leafValue)
+
+            if (TreeGetBalanceFactor(Node->Left) >= 0)
             {
                 //Left Left case
                 if (0 != TreeRotateRight(Tree, Node))
@@ -347,9 +539,9 @@ static int TreeBalance(CC_TREE *Tree, CC_TREE_NODE *Node)
                 }
             }
         }
-        else if(balance < -1)
+        else if (balance < -1)
         {
-            if (Node->Right->Value >= leafValue)
+            if (TreeGetBalanceFactor(Node->Right) > 0)
             {
                 //Right Left case
                 if (0 != TreeRotateRight(Tree, Node->Right))
@@ -375,6 +567,11 @@ static int TreeBalance(CC_TREE *Tree, CC_TREE_NODE *Node)
     }
 
     return 0;
+}
+
+int TreeGetBalanceFactor(CC_TREE_NODE * Node)
+{
+    return TreeGetNodeHeight(Node->Left) - TreeGetNodeHeight(Node->Right);
 }
 
 static int Max(int Num1, int Num2)
