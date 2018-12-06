c17cbdf1d3e74af640625fd90cfb144eb384482f
Katona Áron <33594498+katonaaron@users.noreply.github.com>
2018-12-06 19:00:00 +0200

    Stack implementation
    -The required functions of the stack were implemented

--- data_struct/ccstack.c
+++ data_struct/ccstack.c
@@ -3,58 +3,168 @@
 
 int StCreate(CC_STACK **Stack)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    return -1;
+    if (NULL == Stack)
+    {
+        return -1;
+    }
+
+    PCC_STACK stack = (PCC_STACK)malloc(sizeof(CC_STACK));
+
+    if (NULL == stack)
+    {
+        *Stack = NULL;
+        return -1;
+    }
+
+    if (0 != VecCreate(&stack->Items))
+    {
+        free(stack);
+        *Stack = NULL;
+        return -1;
+    }
+
+    *Stack = stack;
+    return 0;
 }
 
 int StDestroy(CC_STACK **Stack)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    return -1;
+    if (NULL == Stack)
+    {
+        return -1;
+    }
+
+    if (0 != VecDestroy(&(*Stack)->Items))
+    {
+        return -1;
+    }
+
+    free(*Stack);
+    *Stack = NULL;
+    return 0;
 }
 
 int StPush(CC_STACK *Stack, int Value)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Stack)
+    {
+        return -1;
+    }
+    return VecInsertTail(Stack->Items, Value);
 }
 
 int StPop(CC_STACK *Stack, int *Value)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if (NULL == Stack)
+    {
+        return - 1;
+    }
+
+    int foundVal;
+    int index = VecGetCount(Stack->Items);
+
+    if (index < 0)
+    {
+        return -1;
+    }
+
+    if (0 != StPeek(Stack, &foundVal))
+    {
+        return -1;
+    }
+    
+    if (0 != VecRemoveByIndex(Stack->Items, --index))
+    {
+        return -1;
+    }
+
+    *Value = foundVal;
+    return 0;
 }
 
 int StPeek(CC_STACK *Stack, int *Value)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    CC_UNREFERENCED_PARAMETER(Value);
-    return -1;
+    if(NULL == Stack || NULL == Value)
+    {
+        return -1;
+    }
+
+    int foundVal;
+    int index = VecGetCount(Stack->Items);
+
+    if (index < 0)
+    {
+        return -1;
+    }
+
+    if (0 != VecGetValueByIndex(Stack->Items, --index, &foundVal))
+    {
+        return -1;
+    }
+    
+    *Value = foundVal;
+    return 0;
 }
 
 int StIsEmpty(CC_STACK *Stack)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    return -1;
+    if (NULL == Stack)
+    {
+        return -1;
+    }
+    int count = StGetCount(Stack);
+    if (count < 0)
+    {
+        return -1;
+    }
+    return count == 0;
 }
 
 int StGetCount(CC_STACK *Stack)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    return -1;
+    if (NULL == Stack)
+    {
+        return -1;
+    }
+    return VecGetCount(Stack->Items);
 }
 
 int StClear(CC_STACK *Stack)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    return -1;
+    if (NULL == Stack)
+    {
+        return -1;
+    }
+    return VecClear(Stack->Items);
 }
 
 int StPushStack(CC_STACK *Stack, CC_STACK *StackToPush)
 {
-    CC_UNREFERENCED_PARAMETER(Stack);
-    CC_UNREFERENCED_PARAMETER(StackToPush);
-    return -1;
+    if (NULL == Stack || NULL == StackToPush)
+    {
+        return -1;
+    }
+
+    int top;
+    int isEmpty = StIsEmpty(StackToPush);
+
+    while (0 == isEmpty)
+    {
+        if (0 != StPop(StackToPush, &top))
+        {
+            return -1;
+        }
+        if (0 != StPush(Stack, top))
+        {
+            return -1;
+        }
+        isEmpty = StIsEmpty(StackToPush);
+    }
+
+    if (isEmpty < 0)
+    {
+        return -1;
+    }
+
+    return 0;
 }
